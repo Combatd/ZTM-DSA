1ce49cb1c2517e034da9ff73b7a45d5648ca9fc5
Combatd <combatdstar@gmail.com>
2022-09-20 03:52:16 -0700

    Define #boo

---
+++ bigo/space.js
@@ -0,0 +1,9 @@
+// Time: O(n)
+// Space: O(1) - no additional data structures were created outside of the argument
+function boo(n) {
+  for (let i = 0; i < n.length; i++) {
+    console.log("Boooo!");
+  }
+}
+
+boo([1,2,3,4,5]);
