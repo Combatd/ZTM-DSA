731dac3c12a4837a41632d8dde334470bd4ecdb6
Combatd <combatdstar@gmail.com>
2022-09-20 03:55:57 -0700

    Define #arrayOfHiNTimes

--- bigo/space.js
+++ bigo/space.js
@@ -7,3 +7,15 @@ function boo(n) {
 }
 
 boo([1,2,3,4,5]);
+
+// Time: O(n) We loop through argument n
+// Space: O(n) We initialize a new array and push up to n number of elements
+function arrayOfHiNTimes(n) {
+  let hiArray = [];
+  for (let i = 0; i < n; i++) {
+    hiArray[i] = 'hi';
+  }
+  return hiArray;
+}
+
+arrayOfHiNTimes(6);
