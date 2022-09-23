35192185fd045447c3139fb9c8d6ca79d00015ad
Combatd <combatdstar@gmail.com>
2022-09-23 07:46:35 -0700

    Define HashTable#get

--- hashTable/HashTable.js
+++ hashTable/HashTable.js
@@ -19,8 +19,22 @@ class HashTable {
     this.data[address].push([key, value]);
     return this.data;
   }
+
+  get(key) {
+    let address = this._hash(key);
+    const currentBucket = this.data[address];
+    if (currentBucket) {
+      for (let i = 0; i < currentBucket.length; i++) {
+        if (currentBucket[i][0] === key) {
+          return currentBucket[i][1]; // return the value
+        }
+      }
+    }
+    return undefined;
+  }
 }
 
 const myHashTable = new HashTable(50);
 myHashTable.set('grapes', 10000);
-// myHashTable.get('grapes');
+myHashTable.set('apples', 54);
+myHashTable.get('grapes');
