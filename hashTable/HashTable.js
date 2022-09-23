0c30395bd6b708eb90ff21f98f5bdc43b41e5ea5
Combatd <combatdstar@gmail.com>
2022-09-23 07:51:42 -0700

    Define #keys

--- hashTable/HashTable.js
+++ hashTable/HashTable.js
@@ -32,9 +32,21 @@ class HashTable {
     }
     return undefined;
   }
+
+  keys() {
+    const keysArray = [];
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i]) {
+        keysArray.push(this.data[i][0][0]); // push the key into the keysArray
+      }
+    }
+    return keysArray;
+  }
 }
 
 const myHashTable = new HashTable(50);
 myHashTable.set('grapes', 10000);
 myHashTable.set('apples', 54);
+myHashTable.set('oranges', 2);
 myHashTable.get('grapes');
+console.log(myHashTable.keys());
