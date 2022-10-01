81f5165a583011f6411a7f4edd848b404766f2e8
Combatd <combatdstar@gmail.com>
2022-10-01 14:57:47 -0700

    Create pointers and garbage collect with delete keyword

--- linkedList/intro.js
+++ linkedList/intro.js
@@ -6,3 +6,14 @@ const basket = [
 
 // Apples -> Grapes -> Pears -> (and nothing after)
 // 8947 -> 8742 -> 372 -> null
+
+const obj1 = {
+  a: true
+};
+
+let obj2 = obj1; // pointer/reference to obj1 - not copying the object, pointing to the same memeory location
+obj1.a = 'booya';
+delete obj1
+obj2 = 'hello';
+// console.log(obj1, ' 1');
+console.log(obj2, ' 2');
