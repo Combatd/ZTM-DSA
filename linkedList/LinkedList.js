d2825383586126562fabaffa7de201bbf2ca0633
Combatd <combatdstar@gmail.com>
2022-10-04 09:55:37 -0700

    Define #insert

--- linkedList/LinkedList.js
+++ linkedList/LinkedList.js
@@ -38,9 +38,49 @@ class LinkedList {
     this.length++;
     return this;
   }
+
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while(currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+
+  insert = function(index, value) {
+    const newNode = new Node(value);
+    if (index >= this.length) {
+      return this.append(value);
+    }
+    if(index === 0) {
+      this.prepend(value);
+      return this;
+    }
+    const leader = this.traverseToIndex(index - 1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this;
+  }
 }
 
 const myLinkedList = new LinkedList(10);
-console.log(myLinkedList.append(11));
-console.log(myLinkedList.append(12));
+console.log(myLinkedList.append(5));
+console.log(myLinkedList.append(16));
 console.log(myLinkedList.prepend(1));
+console.log(myLinkedList.insert(2, 99));
+console.log(myLinkedList.insert(20, 88));
+console.log(myLinkedList.printList());
