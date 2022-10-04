587ab8baa864d0b44f42a042fc5eb1a73040725f
Combatd <combatdstar@gmail.com>
2022-10-04 09:30:37 -0700

    Create Node class

--- linkedList/LinkedList.js
+++ linkedList/LinkedList.js
@@ -1,3 +1,10 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
 class LinkedList {
   constructor(value) {
     this.head = {
@@ -9,10 +16,11 @@ class LinkedList {
   }
 
   append = function(value) {
-    const newNode = {
-      value: value,
-      next: null
-    };
+    // const newNode = {
+    //   value: value,
+    //   next: null
+    // };
+    const newNode = new Node(value);
     this.tail.next = newNode;
     this.tail = newNode;
     this.length++;
@@ -20,10 +28,11 @@ class LinkedList {
   }
 
   prepend(value) {
-    const newNode = {
-      value: value,
-      next: null
-    }
+    // const newNode = {
+    //   value: value,
+    //   next: null
+    // }
+    const newNode = new Node(value);
     newNode.next = this.head;
     this.head = newNode;
     this.length++;
