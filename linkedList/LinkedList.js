4b1b24690124f0ab39cde6bf712f187a383e3371
Combatd <combatdstar@gmail.com>
2022-10-04 09:12:54 -0700

    Define #prepend

--- linkedList/LinkedList.js
+++ linkedList/LinkedList.js
@@ -18,8 +18,20 @@ class LinkedList {
     this.length++;
     return this;
   }
+
+  prepend(value) {
+    const newNode = {
+      value: value,
+      next: null
+    }
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
 }
 
 const myLinkedList = new LinkedList(10);
 console.log(myLinkedList.append(11));
 console.log(myLinkedList.append(12));
+console.log(myLinkedList.prepend(1));
