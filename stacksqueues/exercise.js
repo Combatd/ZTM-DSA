eaf6d15986d4719a8b5d736b7712574b8277dd9b
Combatd <combatdstar@gmail.com>
2022-10-05 15:34:43 -0700

    Take notes on Stacks and Queues"

---
+++ stacksqueues/exercise.js
@@ -0,0 +1,20 @@
+// Stacks
+
+// Google
+// Udemy
+// YouTube
+
+// Arrays - Both would work, since we can pop off the end of the array, but arrays are easier to implement, arrays will have to double moemory available as previous memory capacity is reached
+// Linked Lists - More memory might be used to hold pointers, but more dynamic memory is available
+
+
+
+// Queues
+
+// Head - Matt
+// Tail - Pavel
+// O(1) operation to get head and tail
+// Matt -- Joy -- Samir -- Pavel
+
+// Arrays
+// Linked List - Use this because we don't want to constantly "resave" arrays if we take data from the front of an array
