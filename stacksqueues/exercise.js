4d5ea79daa6d8cde0fb274489e015ec6286f6db4
Combatd <combatdstar@gmail.com>
2022-10-05 15:51:46 -0700

    Comment on memory heap and call stack

--- stacksqueues/exercise.js
+++ stacksqueues/exercise.js
@@ -18,3 +18,30 @@
 
 // Arrays
 // Linked List - Use this because we don't want to constantly "resave" arrays if we take data from the front of an array
+
+/*
+What is a Program
+
+- Allocate Memory
+- Parse and Execute
+
+Javascript Engine
+- Memory Heap - Memory allocation happens
+- Call Stack - Code is read and executed
+
+
+*/
+
+const a = 1;
+const b = 10;
+const c = 100;
+// When we assign this, JavaScript engine will remember that a has a value of 1
+// Memory Leak
+// With these global variables, we have a limited amount of variables that can be declared
+// Memory leaks happen when we have unused memory laying around filling up the memory heap
+// That's why we say global variables are bad
+
+// Call Stack
+console.log('1');
+console.log('2');
+console.log('3');
