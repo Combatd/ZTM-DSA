1f829c4667770fc6c711e512a2ea02ee5a447542
Combatd <combatdstar@gmail.com>
2022-10-05 16:50:52 -0700

    Create Stack class

---
+++ stacksqueues/Stack.js
@@ -0,0 +1,54 @@
+class Node {
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor(){
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+
+  peek() {
+    return this.top;
+  }
+
+  push(value){
+    const newNode = new Node(value);
+    if (this.isEmpty()) {
+      this.top = newNode;
+      this.bottom = this.top;
+    } else {
+      const holdingPointer = this.top;
+      this.top = newNode;
+      this.top.next = holdingPointer;
+    }
+    this.length++;
+    return newNode;
+  }
+
+  pop(){
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    // const popNode = this.top;
+    this,top = this.top.next
+    this.length--;
+    return this;
+  }
+
+  isEmpty() {
+    return this.length === 0;
+  }
+}
+
+const myStack = new Stack();
+myStack.push('google');
+myStack.push('udemy');
+myStack.push('discord');
+myStack.peek();
+myStack.pop();
+myStack.pop();
