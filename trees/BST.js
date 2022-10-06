50c68c8554f84034956fdc4302d0f6308dffcaff
Combatd <combatdstar@gmail.com>
2022-10-06 15:06:08 -0700

    Define BinarySearchTree#insert

--- trees/BST.js
+++ trees/BST.js
@@ -10,9 +10,33 @@ class BinarySearchTree {
   constructor(){
     this.root = null;
   }
+
   insert(value){
-    //Code here
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if (value < currentNode.value) {
+          // left
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        } else {
+          // right
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+    }
   }
+
   lookup(value){
     //Code here
   }
