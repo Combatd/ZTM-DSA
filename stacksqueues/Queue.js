af22fcc3713888b953ee58a4fb32ddd4109f5ebe
Combatd <combatdstar@gmail.com>
2022-10-06 10:33:32 -0700

    Define #enqueue

--- stacksqueues/Queue.js
+++ stacksqueues/Queue.js
@@ -11,13 +11,34 @@ class Queue {
     this.last = null;
     this.length = 0;
   }
+
   peek() {
+    return this.first;
   }
+
   enqueue(value){
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.length++;
+    return this;
   }
+
   dequeue(){
   }
   //isEmpty;
 }
 
 const myQueue = new Queue();
+myQueue.peek();
+myQueue.enqueue('Joy');
+myQueue.enqueue('Matt');
+myQueue.enqueue('Pavel');
+myQueue.enqueue('Samir');
+
+
