dec389146ce2f9f835c3ee5d74c2ee93f405c310
Combatd <combatdstar@gmail.com>
2022-10-06 13:11:50 -0700

    Comment Binary Tree

--- trees/BinaryTree.js
+++ trees/BinaryTree.js
@@ -3,3 +3,11 @@ function BinaryTreeNode(value) {
   this.left = null;
   this.right = null;
 }
+
+// Level 0: 2^0 = 1
+// Level 1: 2^1 = 2
+// Level 2: 2^2 = 4
+// Level 3: 2^3 = 8
+
+// number of nodes = 2^h - 1, where h starts at 1
+// log nodes = height
