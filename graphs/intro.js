24c978d8be6c52aa0a877f7f3a002862ae36ba94
Combatd <combatdstar@gmail.com>
2022-10-07 11:17:25 -0700

    Declare a bunch of graphs

---
+++ graphs/intro.js
@@ -0,0 +1,23 @@
+// Edge List
+const graph = [
+  [0, 2], 
+[2, 3], 
+[2, 1], 
+[1, 3]
+];
+
+// Adjacency List
+const adjacencyList = [
+  [2],
+  [2, 3],
+  [0, 1, 3],
+  [1, 2]
+];
+
+// Adjacent Matrix
+const adjacentMatrix = [
+  [0, 0, 1, 0],
+  [0, 0, 1, 1],
+  [1, 1, 0, 1],
+  [0, 1, 1, 0]
+];
