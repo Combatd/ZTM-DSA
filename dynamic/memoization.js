1f64d0607b4276b0373f167664ac455f16ed7428
Combatd <combatdstar@gmail.com>
2022-10-15 13:06:33 -0700

    Define anon function as closure

--- dynamic/memoization.js
+++ dynamic/memoization.js
@@ -7,13 +7,15 @@ addTo80(5);
 addTo80(5);
 addTo80(5);
 
-let cache = {};
-function memoizedAddTo80(n) {
-  if (n in cache) { // if cache[n]
-    return cache[n];
-  } else {
-    cache[n] = n + 80;
-    return cache[n];
+function memoizedAddTo80() {
+  let cache = {};
+  return function(n) {
+    if (n in cache) { // if cache[n]
+      return cache[n];
+    } else {
+      cache[n] = n + 80;
+      return cache[n];
+    }
   }
 }
 
@@ -21,6 +23,11 @@ console.log(memoizedAddTo80(5)); // we cache the property 5 to have 85 { 5 : 85 
 console.log(memoizedAddTo80(5)); 
 console.log(memoizedAddTo80(6)); // hey, this wasn't cached yet!
 // simple property access is O(1) time
+
+const memoized = memoizedAddTo80(); // the closure will return the anonymous function to have access the cache inside
+
+console.log('1', memoized(5));
+console.log('2', memoized(5));
 
 /*
 Memoization is a specific form of caching that involves caching a return value based on parameters
